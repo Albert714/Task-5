4a43d7560694884f5af761928691ee8d7d7b273a
Albert714 <albertkasat@gmail.com>
2024-03-09 18:33:50 +0300

    Добавьте файлы проекта.

---
+++ Task 5/Task_5.py
@@ -0,0 +1,13 @@
+number = int(input())
+if number > 0:
+    if number % 2 == 0:
+        print('Положительное четное число')
+    else: 
+        print('Положительное нечетное число')
+elif number < 0:
+    if number % 2 == 0:
+        print('Отрицательное четное число')
+    else: 
+        print('Отрицательное нечетное число')
+else: 
+    print('Нулевое число')
